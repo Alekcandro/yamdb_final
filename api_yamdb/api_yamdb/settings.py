bde18a86205b64ff260fb9d12a9e226e495c010e
Александр Михайлов <alksdr.mih@gmail.com>
2023-05-27 17:35:48 +0300

    settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -126,4 +126,3 @@ AUTH_USER_MODEL = 'users.User'
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 DEFAULT_FROM_EMAIL = 'admin@yatube.com'
-
